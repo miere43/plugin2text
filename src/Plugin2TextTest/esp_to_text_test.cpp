339db8054eb1293570927c179b831bdf12d03c57
miere43 <x.miere@gmail.com>
2021-08-28 21:17:03 +0300

    ESP to text: added byte array RLE reading past buffer regression test.

---
+++ src/Plugin2TextTest/esp_to_text_test.cpp
@@ -0,0 +1,23 @@
+#include <CppUnitTest.h>
+#include <esp_to_text.hpp>
+#include "test_common.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EspToTextTest
+{
+    TEST_CLASS(RegressionTest) {
+    public:
+        TEST_METHOD(Test_ByteArrayRLE_ReadingPastProvidedBuffer) {
+            TextRecordWriter writer;
+            writer.init();
+            defer(writer.dispose());
+
+            const char zeros[15] = "\00\00\00\00\00\00\00\00\00\00\00\00\00\00";
+            writer.write_type(&Type_ByteArrayRLE, zeros, 10); // pass only 10 zeros out of 15.
+            writer.write_bytes("\0", 1); // zero-terminate string.
+
+            Assert::AreEqual("  ?*\n", (const char*)writer.output_buffer.start, L"reading past provided buffer");
+        }
+    };
+}
